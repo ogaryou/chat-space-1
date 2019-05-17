10e0b65d441ca9102fd1cf594e2cbde325391e68
y-wakugami <renesis2b@gmail.com>
2019-05-17 11:27:35 +0900

    fix 自動更新をメッセージページのみで発火するようにif分を追加した

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -45,28 +45,55 @@ $(function(){
   })
 
   //自動更新
-  function reloadMessages(){
-    var last_message_id = $('.timeline__bodyList').last().data('id');
-    var href = 'api/messages'
-    $.ajax({
-    url: href,
-    type: 'GET',
-    data:{id: last_message_id},
-    dataType: 'json'
-    })
+  // function reloadMessages(){
+  //   var last_message_id = $('.timeline__bodyList').last().data('id');
+  //   var href = 'api/messages'
+  //   $.ajax({
+  //   url: href,
+  //   type: 'GET',
+  //   data:{id: last_message_id},
+  //   dataType: 'json'
+  //   })
 
-    .done(function(messages){
-      messages.forEach(function(message){
-        var insertHTML = buildHTML(message)
-        $('#message').append(insertHTML)
-      });
+  //   .done(function(messages){
+  //     console.log(messages);
+  //     messages.forEach(function(message){
+  //       var insertHTML = buildHTML(message)
+  //       $('#message').append(insertHTML)
+  //     });
 
+  //     $('.timeline__body').animate({scrollTop: $('.timeline__body')[0].scrollHeight}, 'fast');
+  //   })
+
+  //   .fail(function(){
+  //     console.log('error');
+  //   });
+  // };
+  // setInterval(reloadMessages, 5000);
+
+  var interval = setInterval(function() {
+    if (location.href.match(/\/groups\/\d+\/messages/)){
       $('.timeline__body').animate({scrollTop: $('.timeline__body')[0].scrollHeight}, 'fast');
-    })
-
-    .fail(function(){
-      console.log('error');
-    });
-  };
-  setInterval(reloadMessages, 5000);
+      var last_message_id = $('.timeline__bodyList').last().data('id');
+      var href = 'api/messages'
+      $.ajax({
+        url: href,
+        type: "GET",
+        data: {id: last_message_id},
+        dataType: "json"
+      })
+      .done(function(messages) {
+        messages.forEach(function(message) {
+          var insertHTML = buildHTML(message)
+          $('#message').append(insertHTML)
+          $('.timeline__body').animate({scrollTop: $('.timeline__body')[0].scrollHeight}, 'fast');
+        })
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+      });
+    } else {
+        clearInterval(interval);
+      }
+  } , 5000 );
 })
